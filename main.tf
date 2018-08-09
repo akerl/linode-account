c3eb1a9d412ddb1028a0c672abe8e5c3c30f6e27
Les Aker <me@lesaker.org>
2018-08-08 18:51:39 -0700

    update state

--- main.tf
+++ main.tf
@@ -5,17 +5,34 @@ variable "ssh_key" {
   default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPrm9UdL04EOBdgpluugFsai+PMIqF2tNmVH757JHH3I"
 }
 
-output "test-address" {
-  value = "${module.test.ip_address}"
+output "green-algo-address" {
+  value = "${module.green-algo.ip_address}"
 }
 
-module "test" {
-  source  = "github.com/akerl/terraform-linode-algo"
-  name    = "test"
+output "red-algo-address" {
+  value = "${module.red-algo.ip_address}"
+}
+
+module "green-algo" {
+  source = "github.com/akerl/terraform-linode-algo"
+
+  name    = "green"
+  ssh_key = "${var.ssh_key}"
+
+  users = [
+    "charlie",
+    "delta",
+  ]
+}
+
+module "red-algo" {
+  source = "github.com/akerl/terraform-linode-algo"
+
+  name    = "red"
   ssh_key = "${var.ssh_key}"
 
   users = [
     "alfa",
-    "beta",
+    "bravo",
   ]
 }
