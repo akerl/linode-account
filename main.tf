00bee1a750519ebe8d276d9b40657a637a2360f6
Les Aker <me@lesaker.org>
2018-08-26 13:27:47 -0400

    update servers

--- main.tf
+++ main.tf
@@ -5,14 +5,14 @@ variable "ssh_key" {
   default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPrm9UdL04EOBdgpluugFsai+PMIqF2tNmVH757JHH3I"
 }
 
-output "blue-algo-address" {
-  value = "${module.blue-algo.ip_address}"
+output "green-algo-address" {
+  value = "${module.green-algo.ip_address}"
 }
 
-module "blue-algo" {
+module "green-algo" {
   source = "github.com/akerl/terraform-linode-algo"
 
-  name    = "blue"
+  name    = "green"
   ssh_key = "${var.ssh_key}"
 
   users = [
