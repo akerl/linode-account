9eb72beda032990eaf100fd3c06f9e9a777a2486
Les Aker <me@lesaker.org>
2018-10-13 14:43:30 -0400

    update state

--- main.tf
+++ main.tf
@@ -1,19 +1,23 @@
 provider "linode" {}
 
-variable "ssh_key" {
-  type    = "string"
-  default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPrm9UdL04EOBdgpluugFsai+PMIqF2tNmVH757JHH3I"
+variable "ssh_keys" {
+  type = "list"
+
+  default = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPrm9UdL04EOBdgpluugFsai+PMIqF2tNmVH757JHH3I",
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPkiSoS5U0Zef2hRKzZ8jk3KIMT5pAiNTMPXf1HzbGZh",
+  ]
 }
 
-output "green-algo-address" {
-  value = "${module.green-algo.ip_address}"
+output "blue-algo-address" {
+  value = "${module.blue-algo.ip_address}"
 }
 
-module "green-algo" {
+module "blue-algo" {
   source = "github.com/akerl/terraform-linode-algo"
 
-  name    = "green"
-  ssh_key = "${var.ssh_key}"
+  name     = "blue"
+  ssh_keys = "${var.ssh_keys}"
 
   users = [
     "alfa",
