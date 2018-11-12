325558445eb3479538e7b64f36712beb72601663
Les Aker <me@lesaker.org>
2018-11-11 19:17:54 -0500

    add codepad

---
+++ codepad.tf
@@ -0,0 +1,28 @@
+output "codepad_address" {
+  value = "${linode_instance.codepad.ip_address}"
+}
+
+resource "linode_instance" "codepad" {
+  label = "codepad"
+
+  region = "us-east"
+  type   = "g6-standard-4"
+
+  disk {
+    label           = "root"
+    size            = 102400
+    authorized_keys = ["${data.external.ssh_key.result.public_key}"]
+    image           = "linode/arch"
+  }
+
+  config {
+    label  = "default"
+    kernel = "linode/grub2"
+
+    devices {
+      sda = {
+        disk_label = "root"
+      }
+    }
+  }
+}
