49379d3345ee605259e54cf647a9f11738ccc12d
Les Aker <me@lesaker.org>
2019-05-04 04:41:25 +0000

    fix formatting

--- vpn_blue.tf
+++ vpn_blue.tf
@@ -3,15 +3,15 @@ output "blue-vpn-address" {
 }
 
 module "blue-vpn" {
-  source            = "armorfret/wireguard/linode"
-  version           = "0.0.13"
-  name              = "blue"
-  ssh_keys          = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGJ8nnGTRnVJR6Sz6lnYdRchw2Z4S9DFOKTHuJBnMYBS"]
-  region            = "ap-south"
-  stackscript_id    = "${module.stackscript.stackscript_id}"
-  image_id          = "${var.image_id}"
-  users             = ["szeth", "lift", "jasnah", "kaladin"]
-  type              = "g6-standard-1"
+  source         = "armorfret/wireguard/linode"
+  version        = "0.0.13"
+  name           = "blue"
+  ssh_keys       = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGJ8nnGTRnVJR6Sz6lnYdRchw2Z4S9DFOKTHuJBnMYBS"]
+  region         = "ap-south"
+  stackscript_id = "${module.stackscript.stackscript_id}"
+  image_id       = "${var.image_id}"
+  users          = ["szeth", "lift", "jasnah", "kaladin"]
+  type           = "g6-standard-1"
 }
 
 resource "linode_domain_record" "blue" {
--- vpn_gray.tf
+++ vpn_gray.tf
@@ -3,15 +3,15 @@ output "gray-vpn-address" {
 }
 
 module "gray-vpn" {
-  source            = "armorfret/wireguard/linode"
-  version           = "0.0.13"
-  name              = "gray"
-  ssh_keys          = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGJ8nnGTRnVJR6Sz6lnYdRchw2Z4S9DFOKTHuJBnMYBS"]
-  region            = "eu-west"
-  stackscript_id    = "${module.stackscript.stackscript_id}"
-  image_id          = "${var.image_id}"
-  users             = ["szeth", "lift", "jasnah", "kaladin"]
-  type              = "g6-standard-1"
+  source         = "armorfret/wireguard/linode"
+  version        = "0.0.13"
+  name           = "gray"
+  ssh_keys       = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGJ8nnGTRnVJR6Sz6lnYdRchw2Z4S9DFOKTHuJBnMYBS"]
+  region         = "eu-west"
+  stackscript_id = "${module.stackscript.stackscript_id}"
+  image_id       = "${var.image_id}"
+  users          = ["szeth", "lift", "jasnah", "kaladin"]
+  type           = "g6-standard-1"
 }
 
 resource "linode_domain_record" "gray" {
