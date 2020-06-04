973b23e513c9bab7b7d367b3f37dd5614a3fde3c
Les Aker <me@lesaker.org>
2020-06-04 23:51:07 +0000

    Revert "attempt to switch to new versions format"
    This reverts commit 9c43648a990f000a1a044cb6672c69f690b52bda.

--- main.tf
+++ main.tf
@@ -1,1 +1,21 @@
+provider "linode" {
+  version = "1.8.0"
+}
+
+provider "template" {
+  version = "2.1.2"
+}
+
+provider "random" {
+  version = "2.2.1"
+}
+
+provider "null" {
+  version = "2.1.2"
+}
+
+provider "external" {
+  version = "1.2.0"
+}
+
 data "linode_profile" "profile" {}
