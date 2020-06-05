1f65d90bfa52718a2ebb69b00e4eaab929294b78
Les Aker <me@lesaker.org>
2020-06-05 23:14:30 +0000

    update tf version

--- main.tf
+++ main.tf
@@ -1,21 +1,41 @@
+terraform {
+  required_providers {
+    external = {
+      source  = "hashicorp/external"
+      version = "1.2.0"
+    }
+    linode = {
+      source  = "terraform-providers/linode"
+      version = "1.11.2"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "2.1.2"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "2.2.1"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = "2.1.2"
+    }
+  }
+}
+
 provider "linode" {
-  version = "1.8.0"
 }
 
 provider "template" {
-  version = "2.1.2"
 }
 
 provider "random" {
-  version = "2.2.1"
 }
 
 provider "null" {
-  version = "2.1.2"
 }
 
 provider "external" {
-  version = "1.2.0"
 }
 
 data "linode_profile" "profile" {}
