5de07086c37665f3e96ef8cf2f00ea86d30d1bd1
Les Aker <me@lesaker.org>
2023-07-18 19:50:48 -0400

    update state

--- main.tf
+++ main.tf
@@ -8,3 +8,57 @@ terraform {
 }
 
 data "linode_profile" "profile" {}
+
+output "instance_addresses" {
+  value = { for i, x in linode_instance.instances : x.label => x.ip_address }
+}
+
+resource "linode_instance" "instances" {
+  for_each = var.instances
+
+  label  = each.key
+  region = "us-east"
+  type   = each.value.instance_type
+}
+
+resource "linode_instance_disk" "instances" {
+  for_each = var.instances
+
+  label            = "root"
+  linode_id        = linode_instance.instances[each.key].id
+  size             = each.value.disk_size
+  image            = "linode/arch"
+  authorized_users = [data.linode_profile.profile.username]
+}
+
+resource "linode_instance_config" "instances" {
+  for_each = var.instances
+
+  label       = "default"
+  linode_id   = linode_instance.instances[each.key].id
+  root_device = "/dev/sda"
+  kernel      = "linode/grub2"
+  booted      = true
+
+  devices {
+    sda {
+      disk_id = linode_instance_disk.instances[each.key].id
+    }
+  }
+
+  helpers {
+    updatedb_disabled  = false
+    devtmpfs_automount = false
+    distro             = false
+    modules_dep        = false
+    network            = false
+  }
+}
+
+resource "linode_rdns" "foo" {
+  for_each = var.instances
+
+  address = linode_instance.instances[each.key].ip_address
+  rdns    = "${each.key}.${var.domain}"
+}
+
--- variables.tf
+++ variables.tf
@@ -1,1 +1,29 @@
+variable "domain" {
+  type    = string
+  default = "linode.a-rwx.org"
+}
 
+variable "instances" {
+  type = map(object({
+    instance_type = string
+    disk_size     = number
+  }))
+  default = {
+    charts = {
+      instance_type = "g6-nanode-1"
+      disk_size     = 20480
+    },
+    dmz = {
+      instance_type = "g6-standard-1"
+      disk_size     = 20480
+    },
+    codepad = {
+      instance_type = "g6-standard-2"
+      disk_size     = 61440
+    },
+    goat = {
+      instance_type = "g6-nanode-1"
+      disk_size     = 25600
+    },
+  }
+}
